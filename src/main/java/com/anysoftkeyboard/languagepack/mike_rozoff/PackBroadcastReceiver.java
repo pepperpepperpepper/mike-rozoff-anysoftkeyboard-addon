d70b8c3c1e8543c17af1c62f22a23b9b510619ff
Pepper <pepper@localhost>
2025-10-01 12:01:28 -0400

    Add PackBroadcastReceiver for AnySoftKeyboard integration

---
+++ src/main/java/com/anysoftkeyboard/languagepack/mike_rozoff/PackBroadcastReceiver.java
@@ -0,0 +1,5 @@
+package com.anysoftkeyboard.languagepack.mike_rozoff;
+
+import com.anysoftkeyboard.addon.PackBroadcastReceiverBase;
+
+public class PackBroadcastReceiver extends PackBroadcastReceiverBase {}
